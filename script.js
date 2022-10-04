0db31adc7bd6275e52c13313ce0bc070c9d426af
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-10-04 05:36:52 -0300

    add transition on mouseover

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
 // Define grid container
 const container = document.querySelector(".container");
 
-// Define slider elements
-const slider = document.getElementById("myRange");
-const output = document.getElementById("output-value");
-output.innerHTML = slider.value;
-
 // Define grid items
 const cell = document.createElement("div");
 cell.classList.add("cell");
 
 // Variables for setting grid size
-let gridSize = 16;
+let gridSize = window.prompt("Select a grid size 1-100:", 16);
 let numOfElements = gridSize * gridSize;
-
-//  update grid size based on slider change
-slider.addEventListener("change", function () {
-  gridSize = slider.value;
-  setGridSize(this.value);
-});
-
-// update on screen slider value
-slider.oninput = function () {
-  output.innerHTML = this.value;
-};
-
-//Adds specified number of grid items to grid
-function createGrid(numOfElements) {
-  for (i = gridSize; i < numOfElements; i++) {
-    container.appendChild(cell.cloneNode(true));
-  }
-}
 
 // Set grid dimensions
 function setGridSize() {
@@ -39,8 +16,14 @@ function setGridSize() {
     `grid-template: repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr )`
   );
 }
+//Adds specified number of grid items to grid
+function createGrid(numOfElements) {
+  for (i = 0; i < numOfElements; i++) {
+    container.appendChild(cell.cloneNode(true));
+  }
+}
 
-// Adds eventlistener to container
+// change cell color
 function addListener() {
   container.addEventListener("mouseover", function handleEvent(event) {
     event.target.classList.add("color-items");
@@ -50,3 +33,23 @@ function addListener() {
 addListener();
 setGridSize();
 createGrid(numOfElements);
+
+// Future Feature
+
+/*  update grid size based on slider change.
+ slider.addEventListener("change", function () {
+ gridSize = slider.value;
+setGridSize(this.value);
+ });
+*/
+
+/* update on screen slider value.
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
+*/
+
+// Define slider elements
+// const slider = document.getElementById("myRange");
+// const output = document.getElementById("output-value");
+// output.innerHTML = slider.value;
