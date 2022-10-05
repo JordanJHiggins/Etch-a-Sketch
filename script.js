70d279d6e90dd273e3f4c10483ecfb984512dd59
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-10-05 18:22:41 -0300

    fix slider to resize grid

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // Define grid container
 const container = document.querySelector(".container");
 
+// Define slider elements
+const slider = document.getElementById("myRange");
+const output = document.getElementById("output-value");
+output.innerHTML = slider.value;
+
 // Define grid items
 const cell = document.createElement("div");
 cell.classList.add("cell");
 
 // Variables for setting grid size
-let gridSize = 0;
+let gridSize = 16;
 
 //Adds specified number of grid items to grid
 function createGrid(gridSize) {
@@ -17,8 +22,8 @@ function createGrid(gridSize) {
     container.insertAdjacentElement("beforeend", cell.cloneNode(true));
   }
 }
+createGrid(gridSize);
 
-createGrid(12);
 // change cell color
 function addListener() {
   container.addEventListener("mouseover", function handleEvent(event) {
@@ -26,28 +31,15 @@ function addListener() {
   });
 }
 
-function changeGridSize() {
-  gridSize = window.prompt("choose a new grid size 1-100");
-}
-// changeGridButton.addEventListener("click", changeGrid);
 addListener();
 
-// Future Feature
+//  update grid size based on slider change.
+slider.addEventListener("change", function (gridSize) {
+  gridSize = slider.value;
+  createGrid(gridSize);
+});
 
-/*  update grid size based on slider change.
- slider.addEventListener("change", function () {
- gridSize = slider.value;
-setGridSize(this.value);
- });
-*/
-
-/* update on screen slider value.
+//  update on screen slider value.
 slider.oninput = function () {
   output.innerHTML = this.value;
 };
-*/
-
-// Define slider elements
-// const slider = document.getElementById("myRange");
-// const output = document.getElementById("output-value");
-// output.innerHTML = slider.value;
